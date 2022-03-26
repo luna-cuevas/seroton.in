b8d1b325a5fdc378c945d2271437c37a0b8f72de
Luna Cuevas <68247976+luna-cuevas@users.noreply.github.com>
2022-03-26 14:01:43 -0700

    reinitializing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Homepage from './pages/Homepage';
-import Nav from './components/Nav'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Homepage from './pages/Homepage.jsx';
+import Nav from './components/Nav.js'
 import Dogs from './pages/Dogs'
 
 function App() {
   return (
-    <Router>
       <div className='bg-[#f8eddb]'>
         <Nav />
         <section className='h-screen'>
           <div className='flex m-auto justify-center bg-[#fdfbf6] h-5/6 w-5/6 rounded-3xl drop-shadow-2xl'> 
-          <Routes>
-            <Route exact path="/home" element={<Homepage/>}/>
-            <Route exact path="/dogs" element={<Dogs/>}/>
-          </Routes>
+            <Routes>
+              <Route  path="/home" element={<Homepage />}  />
+              <Route  path="/dogs" element={<Dogs/>} />
+            </Routes>
           </div>
         </section>
       </div>
-    </Router>
+
   );
 }
 
